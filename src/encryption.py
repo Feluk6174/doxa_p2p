08eb9fd738443fc0420245745dee8426e202007f
Feluk6174 <feluk6174@proton.me>
2023-08-12 13:52:50 +0200

    Added the option to store keys in other files and added dockstring to the encription module

--- src/encryption.py
+++ src/encryption.py
@@ -5,24 +5,62 @@ from nacl.signing import SigningKey, VerifyKey, SignedMessage
 from nacl.hash import sha256
 
 def gen_keys() -> tuple[SigningKey, VerifyKey]:
+    """Generates keys for signing (Ed25519)
+
+    Returns:
+        tuple[SigningKey, VerifyKey]: Returns a signing key and a verify key
+    """
     signing_key = SigningKey.generate()
     return signing_key, signing_key.verify_key
 
-def store_keys(signing_key:SigningKey) -> None:
-    with open("files/key.bin", "wb") as f:
+def store_keys(signing_key:SigningKey, file:str = "key.bin") -> None:
+    """Stores the keys in a file.
+
+    Args:
+        signing_key (SigningKey): The signing key that needs to be stored. (Verify key is not needed because it derives from the signing key).
+        file (str, optional): Name of the file where the keys should be stored. Defaults to "key.bin".
+    """
+    with open(f"files/{file}", "wb") as f:
         f.write(signing_key.encode())
 
-def get_keys() -> tuple[SigningKey, VerifyKey]:
-    with open("files/key.bin", "rb") as f:
+def get_keys(file:str = "key.bin") -> tuple[SigningKey, VerifyKey]:
+    """Gets keys from stored file.
+
+    Args:
+        file (str, optional): Name of the file where the keys are stored. Defaults to "key.bin".
+
+    Returns:
+        tuple[SigningKey, VerifyKey]: Signing and Verifing key (Ed25519)
+    """
+    with open(f"files/{file}", "rb") as f:
         data = f.read()
     signing_key = SigningKey(data)
     return signing_key, signing_key.verify_key
 
 def sign(message:bytes, signing_key:SigningKey) -> str:
+    """Creates signatre of a message (Ed25519)
+
+    Args:
+        message (bytes): Message that needs to be signed
+        signing_key (SigningKey): The key with which the message will be signed
+
+    Returns:
+        str: Signature encoded in urlsafe_b64 (base64.urlsafe_b64encode)
+    """
     signature = signing_key.sign(message)
     return base64.urlsafe_b64encode(signature.signature).decode("utf-8")
 
 def verify(signature:str, message:bytes, key:VerifyKey|SigningKey) -> bool:
+    """Verifies if the signature of a message is correct (Ed25519)
+
+    Args:
+        signature (str): Signature of the message encoded in urlsafe_b64 (base64.urlsafe_b64encode) 
+        message (bytes): MEssage tht needs to be verified
+        key (VerifyKey | SigningKey): Key to verify the message
+
+    Returns:
+        bool: If the signature is correct it will return true else it will return false
+    """
     if isinstance(key, SigningKey):
         key = key.verify_key
     try:
@@ -32,6 +70,14 @@ def verify(signature:str, message:bytes, key:VerifyKey|SigningKey) -> bool:
         return False
 
 def hash(data:bytes) -> str:
+    """Creates Hash of some data (SHA255)
+
+    Args:
+        data (bytes): Data that needs to be hashed
+
+    Returns:
+        str: hash encoded in urlsafe_b64 (base64.urlsafe_b64encode)
+    """
     return base64.urlsafe_b64encode(sha256(data)).decode("utf-8")
 
 
--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -2,18 +2,23 @@ import pytest
 
 import src.encryption as encryption
 
+key_file = "test_key.bin"
+
 def test_save(keys):
-    encryption.store_keys(keys[0])
-    with open("files/key.bin", "rb") as f:
+    global key_file
+    encryption.store_keys(keys[0], file = key_file)
+    with open(f"files/{key_file}", "rb") as f:
         assert f.read() == keys[0].encode()
 
 def test_load():
-    keys = encryption.get_keys()
-    with open("files/key.bin", "rb") as f:
+    global key_file
+    keys = encryption.get_keys(file = key_file)
+    with open(f"files/{key_file}", "rb") as f:
         assert f.read() == keys[0].encode()
 
 def test_verify():
-    signing_key, verify_key = encryption.get_keys()
+    global key_file
+    signing_key, verify_key = encryption.get_keys(file = key_file)
     signature = encryption.sign(b"Hello world!", signing_key)
     assert encryption.verify(signature, b"Hello world!", verify_key)
     assert encryption.verify(signature, b"Hello world!", signing_key)
